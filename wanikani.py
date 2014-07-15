210902e447440399ef2de8d2debca6cbaf3a074a
Acalewin <acalewin@acals-cave.com>
2014-07-14 20:38:15 -0400

    Fleshed out the basics of what each API method does

--- wanikani.py
+++ wanikani.py
@@ -1,4 +1,3 @@
-import json
 import requests
 
 class User:
@@ -37,35 +36,64 @@ class Wanikani:
 
   def _call(self, resource, arg=None):
     """Consolidating making resource calls to the WK api"""
-    if !self.user: return None
+    if not self.user: return None
     url = "https://www.wanikani.com/api/user/%s/%s" \
     % (self.user.apikey, resource)
-    print(url)
+    if arg:
+      url += "/%s" % arg
+    req = requests.get(url)
+    req.raise_for_status()
+    return req.json()
 
   def user_information(self):
     """User information call"""
-    pass
+    data = self._call(resource="user-information")
+    return data['user_information']
 
   def study_queue(self):
-    pass
+    """Study queue call"""
+    data = self._call(resource="study-queue")
+    return data['requested_information']
 
   def level_progression(self):
-    pass
+    """Level progression call"""
+    data = self._call(resource="level-progression")
+    return data['requested_information']
 
   def srs_distribution(self):
-    pass
+    """SRS distribution call"""
+    data = self._call(resource="srs-distribution")
+    return data['requested_information']
 
   def recent_unlocks(self, limit=10):
-    pass
+    """Recent unlocks
+      limit: max number of items to return"""
+    data = self._call(resource='recent-unlocks', arg=limit)
+    return data['requested_information']
 
   def critical_items(self, thresh=75):
-    pass
+    """Critical items
+      thresh: threshold of percentage correct to use"""
+    data = self._call(resource='critical-items', arg=thresh)
+    return data['requested_information']
 
   def radicals(self, level=None):
-    pass
+    """radicals api call
+      level: level to retrieve radicals from"""
+    data = self._call(resource='radicals', arg=level)
+    return data['requested_information']
 
   def kanji(self, level=None):
-    pass
+    """kanji api call
+      level: level to retrieve kanji from"""
+    data = self._call(resource='kanji', arg=level)
+    return data['requested_information']
 
   def vocab(self, level=None):
-    pass
+    """vocabulary api call
+      level: level to retrieve vocab from"""
+    data = self._call(resource='vocabulary', arg=level)
+    if level: #This needs to be unwrapped, because for some reason
+      return data['requested_information']
+    else:
+      return data['requested_information']['general']
