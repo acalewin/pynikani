3c61a7522dc7c662efac4df66aa72f1ced517a5e
Acalewin <acalewin@acals-cave.com>
2014-07-14 21:55:36 -0400

    Error handling and dependencies in the readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # PYNIKANI
 
-The pynikani module is a simple python wrapper for the [wanikani](http://www.wanikani.com) web api.  It 
+The pynikani module is a simple python wrapper for the [wanikani](http://www.wanikani.com) web api.  It
 was bread out the annoyance of wrapping the same api calls and grabbing the
 same data every time I wanted to write a fun script to do something.
+
+Dependencies:
+- requests
--- wanikani.py
+++ wanikani.py
@@ -34,6 +34,18 @@ class Wanikani:
   def user(self):
     del self._user
 
+  @property
+  def error(self):
+    return self._error
+
+  @error.setter
+  def error(self, nval):
+    self._error = nval
+
+  @user.deleter
+  def error(self):
+    del self._error
+
   def _call(self, resource, arg=None):
     """Consolidating making resource calls to the WK api"""
     if not self.user: return None
@@ -41,9 +53,17 @@ class Wanikani:
     % (self.user.apikey, resource)
     if arg:
       url += "/%s" % arg
-    req = requests.get(url)
-    req.raise_for_status()
-    return req.json()
+    try:
+      req = requests.get(url)
+      req.raise_for_status()
+      data = req.json()
+    except requests.exceptions.ConnectionError:
+      self.error = 'Conn Error'
+      data = None
+    except requests.exceptions.HTTPError as e:
+      self.error = e.response.status_code
+      data = None
+    return data
 
   def user_information(self):
     """User information call"""
@@ -58,42 +78,43 @@ class Wanikani:
   def level_progression(self):
     """Level progression call"""
     data = self._call(resource="level-progression")
-    return data['requested_information']
+    return data.get('requested_information')
 
   def srs_distribution(self):
     """SRS distribution call"""
     data = self._call(resource="srs-distribution")
-    return data['requested_information']
+    return data.get('requested_information')
 
   def recent_unlocks(self, limit=10):
     """Recent unlocks
       limit: max number of items to return"""
     data = self._call(resource='recent-unlocks', arg=limit)
-    return data['requested_information']
+    return data.get('requested_information')
 
   def critical_items(self, thresh=75):
     """Critical items
       thresh: threshold of percentage correct to use"""
     data = self._call(resource='critical-items', arg=thresh)
-    return data['requested_information']
+    return data.get('requested_information')
 
   def radicals(self, level=None):
     """radicals api call
       level: level to retrieve radicals from"""
     data = self._call(resource='radicals', arg=level)
-    return data['requested_information']
+    return data.get('requested_information')
 
   def kanji(self, level=None):
     """kanji api call
       level: level to retrieve kanji from"""
     data = self._call(resource='kanji', arg=level)
-    return data['requested_information']
+    return data.get('requested_information')
 
   def vocab(self, level=None):
     """vocabulary api call
       level: level to retrieve vocab from"""
     data = self._call(resource='vocabulary', arg=level)
-    if level: #This needs to be unwrapped, because for some reason
-      return data['requested_information']
+    retval = data.get('requested_information')
+    if level or not retval: #This needs to be unwrapped, because for some reason
+      return retval
     else:
-      return data['requested_information']['general']
+      return retval.get('general')
